b9d458929589e9f0858b6d644b82db64545205c1
HardBird <67629949+HardBird@users.noreply.github.com>
2021-08-19 18:55:21 +0900

    0819  programmers_ct

---
+++ C++_algorithm/C++_algorithm/programmers_ct_19(위장-iterator).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+
+
+	int answer = 1;
+	map<string, int> m;
+	map<string, int>::iterator i;
+
+	for (auto item : clothes)
+	{
+		m[item[1]]++;
+	}
+	for (i = m.begin(); i != m.end(); i++)
+	{
+		cout << i->first << " ";
+		cout << i->second << endl;
+	}
+	//pair일때도 first,second로 접근한다 
+
+	map<string, int>::iterator it;
+	for (it = m.begin(); it != m.end(); it++) {
+		answer *= it->second + 1;
+	}
+	return answer - 1;
+
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	solution({ {"yellowhat", "headgear"} , {"bluesunglasses", "eyewear"},{"green_turban", "headgear"} });
+
+}
