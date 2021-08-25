f4d78398121e6cd197b6aff8792c83f6c423e983
HardBird <67629949+HardBird@users.noreply.github.com>
2021-08-25 23:47:29 +0900

    0825 programmers_ct

---
+++ C++_algorithm/C++_algorithm/pr(주사위경우의수).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int answer[11];
+int N = 2;
+int tot = 0;
+void dfs(int depth) {
+	int i;
+	tot++;
+	if (depth > N) {
+		for (i = 1; i <= N; i++) {
+			cout << answer[i];
+		}
+		cout << endl;
+		return;
+	}
+	for (int i = 1; i <= 6; i++) {
+		answer[depth] = i;
+		dfs(depth + 1);
+	}
+}
+
+
+int main()
+{
+	dfs(1);
+	cout <<"answer: "<< tot;
+	return 0;
+}
